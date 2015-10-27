434a9ee0ab53d0c51af8631446539fb4d664c06b
Lachlan Miller <lachlanmiller@Lachlans-MacBook-Pro.local>
2015-10-28 03:00:05 +1000

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
 
+  resources :users
+
 end
 
   # The priority is based upon order of creation: first created -> highest priority.
