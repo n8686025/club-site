e976579ba800dec16488c248a732abaffd87e0f5
Lachlan Miller <lachlanmiller@Lachlans-MacBook-Pro.local>
2015-10-27 16:24:33 +1000

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+	# Return the full title on per page basis.
+	# In the case a title is not provided in .html.erb 
+	# using <% provide... %>
+
+	def full_title(page_title = '')
+		base_title = "QUT Japan"
+		if page_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+	end
+	
 end
